313b2929c722296d6da4ad548f64f9b5bb2653aa
Browwniass <95659478+Browwniass@users.noreply.github.com>
2023-09-24 21:42:57 +0300

    Second Com

--- auctions/views.py
+++ auctions/views.py
@@ -3,9 +3,39 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms 
 
-from .models import User
+from .models import User, Listings, Bids, Categories
+ 
 
+class NewListingForm(forms.Form):
+    title = forms.CharField(max_length=100, label="",
+                            widget= forms.TextInput
+                            (attrs={'class':'form-control', 
+                            'placeholder': 'Title'}))
+    bid = forms.IntegerField(min_value=0, label="",
+                           widget= forms.NumberInput
+                           (attrs={'class':'form-control',
+                            'placeholder': 'Bid'}))
+    description = forms.CharField(max_length=400, label="",
+                           widget= forms.Textarea
+                           (attrs={'class':'form-control',
+                            'placeholder': 'Description'}))
+    photo = forms.CharField(max_length=60, label="",
+                           widget= forms.URLInput
+                           (attrs={'class':'form-control',
+                            'placeholder': 'Photo URL'}), required=False)
+    categories = forms.ChoiceField(choices=[
+                            (choice.pk, choice.title) for choice in Categories.objects.all()],
+                            required=False , label="",
+                           widget= forms.Select
+                           (attrs={'class':'form-control',
+                            'placeholder': 'Category'})) 
+    
+    """forms.CharField(max_length=60, label="",
+                           widget= forms.TextInput
+                           (attrs={'class':'form-control',
+                            'placeholder': 'Category'}))"""
 
 def index(request):
     return render(request, "auctions/index.html")
@@ -61,3 +91,33 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create_view(request):
+    current_user = request.user.id
+    
+    if request.method=="POST":
+        
+        form = NewListingForm(request.POST)
+
+        if form.is_valid():
+            print(form.cleaned_data['categories'])
+            listing = Listings(
+                title=form.cleaned_data['title'],
+                description=form.cleaned_data['description'],
+                photo=form.cleaned_data['photo'],
+                author=request.user, 
+                is_active=True,
+                id_categories=Categories.objects.get(pk=form.cleaned_data['categories'])
+            )
+            listing.save()
+            bid = Bids(
+                author=request.user, 
+                listing=listing,
+                price=form.cleaned_data['bid']
+            )
+            bid.save()
+
+
+    return render(request, "auctions/create_listing.html", 
+        {"form":NewListingForm()})
